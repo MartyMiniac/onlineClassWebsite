20edb5f47eff1945e64e5294da7b4280f4f1f1e0
MartyMiniac <rohanvermaara@gmail.com>
2020-07-26 15:55:40 +0530

    added index page

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ app=Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method=='POST':
-        pass
+        try:
+            print(request.form['name'])
+            print(request.form['class'])
+            print(request.form['sections'])
+        except:
+            return render_template('index.html', show_hidden=True)
+        return ""
     else:
-        return render_template('index.html')
+        return render_template('index.html', show_hidden=False)
 
 if __name__=='__main__':
     app.run(debug=True)
